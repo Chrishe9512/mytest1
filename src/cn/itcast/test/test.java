5e0f9d1c29947e1629ffe586383f8bce203ce5cf
chris <chrishe9512@sina.com>
2019-02-28 19:57:39 +0800

    添加test工程至git

---
+++ src/cn/itcast/test/test.java
@@ -0,0 +1,11 @@
+package cn.itcast.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+
+        System.out.println("aaa");
+    }
+}
